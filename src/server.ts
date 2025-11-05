1c1c5e55daab61118c2738286e7f8f95b56f28b6
Alexander Kuzmin <kuzminalex96@icloud.com>
2025-11-05 02:12:11 +0100

    feat: Initial commit, structure created, learn how to use this ts because i still dont feel any advantages of it, only extra time wasting

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import http from "node:http";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env' });
+
+const PORT = Number(process.env.PORT) || 1234;
+
+const host = 'localhost';
+
+const server = http.createServer((req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end("Server Created!");
+});
+
+server.listen(PORT, host, () => {
+	console.log(`Server running at http://${host}:${PORT}`);
+});
