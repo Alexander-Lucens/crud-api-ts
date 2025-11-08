35204d6aa05cca47bb938206a73c512335bfa2e1
Alexander Kuzmin <kuzminalex96@icloud.com>
2025-11-08 13:00:13 +0100

    fix: Split server. Put app creation in separate file and make it possible to get it for tests as well. In server lefts only server initialisation part and downloaded jest+supertest

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import app from "./app.js";
 import dotenv from 'dotenv';
-import { MiniExpress } from "./utils/helper/miniExpress.js";
-import userRouter from './routes/usersRoutes.js';
 
 dotenv.config({ path: './.env' });
 const PORT = Number(process.env.PORT) || 1234;
-
-const app = new MiniExpress();
-
-app.use('/api/users', userRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
